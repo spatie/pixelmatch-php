6009066c4fdc9703f748345cf2abc94a33ca0a11
Freek Van der Herten <freek@spatie.be>
2023-09-11 08:30:09 +0200

    move option methods to pixelmatch class

--- src/Concerns/HasOptions.php
+++ src/Concerns/HasOptions.php
@@ -17,38 +17,4 @@ trait HasOptions
      * Smaller values make the comparison more sensitive
      */
     protected float $threshold;
-
-    public function includeAa(bool $includeAa = true): self
-    {
-        $this->includeAa = $includeAa;
-
-        return $this;
-    }
-
-    public function threshold(float $threshold): self
-    {
-        if ($threshold > 1 || $threshold < 0) {
-            throw new InvalidArgumentException('Threshold should be between 0 and 1');
-        }
-
-        $this->threshold = $threshold;
-
-        return $this;
-    }
-
-    /** @return array<string, mixed> */
-    public function options(): array
-    {
-        $options = [];
-
-        if (isset($this->includeAa)) {
-            $options['includeAA'] = $this->includeAa;
-        }
-
-        if (isset($this->threshold)) {
-            $options['threshold'] = $this->threshold;
-        }
-
-        return $options;
-    }
 }
--- src/Pixelmatch.php
+++ src/Pixelmatch.php
@@ -2,15 +2,24 @@
 
 namespace Spatie\Pixelmatch;
 
+use InvalidArgumentException;
 use Spatie\Pixelmatch\Actions\ExecuteNodeAction;
 use Spatie\Pixelmatch\Concerns\HasOptions;
 use Spatie\Pixelmatch\Enums\Output;
 
 class Pixelmatch
 {
-    use HasOptions;
-
     protected string $workingDirectory;
+
+    /*
+     * If true, we'll ignore anti-aliased pixels
+     */
+    protected bool $includeAa;
+
+    /*
+     * Smaller values make the comparison more sensitive
+     */
+    protected float $threshold;
 
     protected function __construct(
         public string $pathToImage1,
@@ -23,6 +32,40 @@ class Pixelmatch
     public static function new(string $pathToImage1, string $pathToImage2): self
     {
         return new static($pathToImage1, $pathToImage2);
+    }
+
+    public function includeAa(bool $includeAa = true): self
+    {
+        $this->includeAa = $includeAa;
+
+        return $this;
+    }
+
+    public function threshold(float $threshold): self
+    {
+        if ($threshold > 1 || $threshold < 0) {
+            throw new InvalidArgumentException('Threshold should be between 0 and 1');
+        }
+
+        $this->threshold = $threshold;
+
+        return $this;
+    }
+
+    /** @return array<string, mixed> */
+    public function options(): array
+    {
+        $options = [];
+
+        if (isset($this->includeAa)) {
+            $options['includeAA'] = $this->includeAa;
+        }
+
+        if (isset($this->threshold)) {
+            $options['threshold'] = $this->threshold;
+        }
+
+        return $options;
     }
 
     public function matchingPercentage(): int
